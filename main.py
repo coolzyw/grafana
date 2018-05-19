6733d70a6ecb29bc7de45b2dc7e309dda73580de
happyzyw <happyzyw@umich.edu>
2018-05-19 13:02:42 -0400

    table

--- main.py
+++ main.py
@@ -12,4 +12,15 @@ table1 = (
     "  PRIMARY KEY (`dept_no`), UNIQUE KEY `dept_name` (`dept_name`)"
     ") ENGINE=InnoDB")
 
+table2 =  ("CREATE TABLE `employees` ("
+    "  `emp_no` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `birth_date` date NOT NULL,"
+    "  `first_name` varchar(14) NOT NULL,"
+    "  `last_name` varchar(16) NOT NULL,"
+    "  `gender` enum('M','F') NOT NULL,"
+    "  `hire_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`)"
+    ") ENGINE=InnoDB")
+
 curr.execute(table1)
+curr.execute(table2)
