7264035f3b6f35b933005926a99e951ba16067af
coolzyw <happyzyw@umich.edu>
2018-06-12 12:13:29 -0400

    Update test1.js

--- main_page/test1.js
+++ main_page/test1.js
@@ -1,32 +1,10 @@
-window.onload = function () {
+var canvas = document.getElementById('canvas');
+      if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
 
-    var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        theme: "light2",
-        title:{
-            text: "Simple Line Chart"
-        },
-        axisY:{
-            includeZero: false
-        },
-        data: [{        
-            type: "line",       
-            dataPoints: [
-                { y: 450 },
-                { y: 414},
-                { y: 520, indexLabel: "highest",markerColor: "red", markerType: "triangle" },
-                { y: 460 },
-                { y: 450 },
-                { y: 500 },
-                { y: 480 },
-                { y: 480 },
-                { y: 410 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
-                { y: 500 },
-                { y: 480 },
-                { y: 510 }
-            ]
-        }]
-    });
-    chart.render();
-    
-    }
+        ctx.fillStyle = 'rgb(200, 0, 0)';
+        ctx.fillRect(10, 10, 50, 50);
+
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillRect(30, 30, 50, 50);
+      }
